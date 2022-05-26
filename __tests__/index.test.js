e099959226bebc7f4b324c48bfed75f4a60d1a22
lev33 <48180783+lev33@users.noreply.github.com>
2022-05-26 08:45:00 +0300

    fix linter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -36,7 +36,7 @@ describe('string', () => {
     expect(schema.minLength(0).isValid('')).toBe(true);
     expect(schema.minLength(1).isValid('')).toBe(false);
     expect(schema.minLength(1).isValid(null)).toBe(false);
-    expect(schema.minLength(1).isValid('what')).toBe(true);   
+    expect(schema.minLength(1).isValid('what')).toBe(true);
   });
 });
 
@@ -48,19 +48,19 @@ describe('number', () => {
     expect(schema.isValid(null)).toBe(true);
     expect(schema.isValid(0)).toBe(true);
   });
-  
+
   test('required', async () => {
     const v = new Validator();
     const schema = v.number().required();
 
     expect(schema.isValid(null)).toBe(false);
     expect(schema.isValid(7)).toBe(true);
-    });
-    
+  });
+
   test('positive', async () => {
     const v = new Validator();
     const schema = v.number().positive();
-  
+
     expect(schema.isValid(10)).toBe(true);
     expect(schema.isValid(0)).toBe(false);
     expect(schema.isValid(-5)).toBe(false);
@@ -69,7 +69,7 @@ describe('number', () => {
   test('range', async () => {
     const v = new Validator();
     const schema = v.number().range(-5, 5);
-  
+
     expect(schema.isValid(-3)).toBe(true);
     expect(schema.isValid(5)).toBe(true);
     expect(schema.isValid(7)).toBe(false);
@@ -78,17 +78,17 @@ describe('number', () => {
 });
 
 describe('array', () => {
-  test('test array', async () => {
+  test('test_array', async () => {
     const v = new Validator();
     const schema = v.array();
-  
+
     expect(schema.isValid(null)).toBe(true);
-  
+
     schema.required();
     expect(schema.isValid(null)).toBe(false);
     expect(schema.isValid([])).toBe(true);
     expect(schema.isValid(['hexlet'])).toBe(true);
-  
+
     schema.sizeof(2);
     expect(schema.isValid(['hexlet'])).toBe(false);
     expect(schema.isValid(['hexlet', 'code-basics'])).toBe(true);
@@ -96,15 +96,15 @@ describe('array', () => {
 });
 
 describe('object', () => {
-  test('test object', async () => {
+  test('test_object', async () => {
     const v = new Validator();
     const schema = v.object();
-  
+
     schema.shape({
       name: v.string().required(),
       age: v.number().positive(),
     });
-  
+
     expect(schema.isValid({ name: 'kolya', age: 100 })).toBe(true);
     expect(schema.isValid({ name: 'maya', age: null })).toBe(true);
     expect(schema.isValid({ name: '', age: null })).toBe(false);
@@ -115,10 +115,10 @@ describe('object', () => {
 describe('validators', () => {
   test('startWith', async () => {
     const v = new Validator();
-  
+
     const fn = (value, start) => value.startsWith(start);
     v.addValidator('string', 'startWith', fn);
-  
+
     const schema = v.string().test('startWith', 'H');
     expect(schema.isValid('exlet')).toBe(false);
     expect(schema.isValid('Hexlet')).toBe(true);
--- src/index.js
+++ src/index.js
@@ -1,143 +1,143 @@
 class Validator {
-    constructor(self = {}) {
-      const {
-        type = null, required = false, minLength = null, contains = null,
-        positive = false, min = null, max = null, range = false,
-        sizeof = null,
-        shape = {},
-        validators = [], test = {},
-        } = self.options || {};
-      this.options = { type, required, minLength, contains, positive, min, max, range, sizeof, shape, validators, test };
-    }
-  
-    string() {
-      this.options.type = 'string';
-      return new Validator(this);
-    }
+  constructor(self = {}) {
+    const {
+      type = null, required = false, minLength = null, contains = null,
+      positive = false, min = null, max = null, range = false,
+      sizeof = null,
+      shape = {},
+      validators = [], test = {},
+    } = self.options || {};
+    this.options = { type, required, minLength, contains, positive, min, max, range, sizeof, shape, validators, test };
+  }
 
-    number() {
-      this.options.type = 'number';
-      return new Validator(this);
+  string() {
+    this.options.type = 'string';
+    return new Validator(this);
+  }
+
+  number() {
+    this.options.type = 'number';
+    return new Validator(this);
+  }
+
+  array() {
+    this.options.type = 'array';
+    return new Validator(this);
+  }
+
+  object() {
+    this.options.type = 'object';
+    return new Validator(this);
+  }
+
+  required() {
+    this.options.required = true;
+    return new Validator(this);
+  }
+
+  contains(value) {
+    this.options.contains = value;
+    return new Validator(this);
+  }
+
+  minLength(value) {
+    this.options.minLength = value;
+    return new Validator(this);
+  }
+
+  positive() {
+    this.options.positive = true;
+    return new Validator(this);
+  }
+
+  range(min, max) {
+    this.options.min = min;
+    this.options.max = max;
+    this.options.range = true;
+    return new Validator(this);
+  }
+
+  sizeof(value) {
+    this.options.sizeof = value;
+    return new Validator(this);
+  }
+
+  shape(obj) {
+    this.options.shape = obj;
+    return new Validator(this);
+  }
+
+  addValidator(type, name, fn) {
+    this.options.validators.push({ type, name, fn });
+    return new Validator(this);
+  }
+
+  test(name, value) {
+    this.options.test[name] = value;
+    return new Validator(this);
+  }
+
+  isValid(value) {
+    if (this.options.required) {
+      if (value === null || value === undefined || value === '') {
+        return false;
       }
-
-    array() {
-      this.options.type = 'array';
-      return new Validator(this);
-    }
-    
-    object() {
-      this.options.type = 'object';
-      return new Validator(this);
-    }
-    
-    required() {
-      this.options.required = true;
-      return new Validator(this);
-    }
-  
-    contains(value) {
-      this.options.contains = value;
-      return new Validator(this);
-    }
-  
-    minLength(value) {
-      this.options.minLength = value;
-      return new Validator(this);
     }
 
-    positive() {
-      this.options.positive = true;
-      return new Validator(this);
+    if (this.options.type === 'string') {
+      if (typeof value !== 'string' && value !== null && value !== undefined) {
+        return false;
       }
-    
-    range(min, max) {
-      this.options.min = min;
-      this.options.max = max;
-      this.options.range = true;
-      return new Validator(this);
-      }
-
-    sizeof(value) {
-      this.options.sizeof = value;
-      return new Validator(this);
-    }
-    
-    shape(obj) {
-      this.options.shape = obj;
-      return new Validator(this);
-    }
-    
-    addValidator(type, name, fn) {
-      this.options.validators.push({ type, name, fn });
-      return new Validator(this);
-    }
-    
-    test(name, value) {
-      this.options.test[name] = value;
-      return new Validator(this);
-    }
-    
-    isValid(value) {
-      if (this.options.required) {
-        if (value === null || value === undefined || value === '') {
+      if (this.options.minLength) {
+        if (value === null || value === undefined || value.length < this.options.minLength) {
           return false;
         }
       }
-
-      if (this.options.type === 'string') {
-        if (typeof value !== 'string' && value !== null && value !== undefined) {
+      if (this.options.contains) {
+        if (value === null || value === undefined || !value.includes(this.options.contains)) {
           return false;
         }
-        if (this.options.minLength) {
-          if (value === null || value === undefined || value.length < this.options.minLength) {
-            return false;
-          }
-        }
-        if (this.options.contains) {
-          if (value === null || value === undefined || !value.includes(this.options.contains)) {
-            return false;
-          }
-        }
       }
-
-      if (this.options.type === 'number') {
-        if (typeof value !== 'number' && value !== null) {
-          return false;
-        }
-        if (this.options.positive) {
-          if (value < 1 && value !== null) {
-            return false;
-          }
-        }
-        if (this.options.range) {
-          if (value > this.options.max) {
-            return false;
-          }
-          if (value < this.options.min) {
-            return false;
-          }
-        }
-      }
-
-      if (this.options.type === 'array') {
-        if (!Array.isArray(value) && value !== null) {
-          return false;
-        }
-        if (this.options.sizeof) {
-          if (value === null || value.length < this.options.sizeof) {
-            return false;
-          }
-        }
-      }
-
-      if (this.options.type === 'object') {
-        const keys = Object.keys(this.options.shape);
-        return keys.every(key => this.options.shape[key].isValid(value[key]));
-      }
-
-      const typeValidators = this.options.validators.filter((validator) => validator.type === this.options.type);
-      return typeValidators.every(({ name, fn }) => Object.keys(this.options.test).includes(name) ? fn(value, this.options.test[name]) : true );
     }
+
+    if (this.options.type === 'number') {
+      if (typeof value !== 'number' && value !== null) {
+        return false;
+      }
+      if (this.options.positive) {
+        if (value < 1 && value !== null) {
+          return false;
+        }
+      }
+      if (this.options.range) {
+        if (value > this.options.max) {
+          return false;
+        }
+        if (value < this.options.min) {
+          return false;
+        }
+      }
+    }
+
+    if (this.options.type === 'array') {
+      if (!Array.isArray(value) && value !== null) {
+        return false;
+      }
+      if (this.options.sizeof) {
+        if (value === null || value.length < this.options.sizeof) {
+          return false;
+        }
+      }
+    }
+
+    if (this.options.type === 'object') {
+      const keys = Object.keys(this.options.shape);
+      return keys.every(key => this.options.shape[key].isValid(value[key]));
+    }
+
+    const typeValidators = this.options.validators.filter((validator) => validator.type === this.options.type);
+    return typeValidators.every(({ name, fn }) => Object.keys(this.options.test).includes(name) ? fn(value, this.options.test[name]) : true);
+  }
 }
 
 export default Validator;
